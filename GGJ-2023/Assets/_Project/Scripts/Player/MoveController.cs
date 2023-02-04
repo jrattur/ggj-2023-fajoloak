2493299f2c08e936a9dbb7e777d8466ce556f96b
gomaoil <foluken@gmail.com>
2023-02-04 15:46:30 +0900

    Ajust direction to player move. But it's 2D and not true move.

--- GGJ-2023/Assets/_Project/Scripts/Player/MoveController.cs
+++ GGJ-2023/Assets/_Project/Scripts/Player/MoveController.cs
@@ -7,37 +7,66 @@ public class MoveController : MonoBehaviour
     [SerializeField]
     float _speedByInput;
     [SerializeField]
+    float _rotationSpeedDeg;
+    [SerializeField]
     bool _isControlByArrow = false;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        _position = this.transform.position;
+        _rotation = this.transform.localRotation;
     }
 
     // Update is called once per frame
     void Update()
     {
-        moveVelocity = Vector3.zero;
+        _moveVelocity = Vector3.zero;
 
         if (Input.GetKey(_isControlByArrow ? KeyCode.DownArrow : KeyCode.S)) {
-            moveVelocity.y -= _speedByInput;
+            _moveVelocity.y -= _speedByInput;
         }
         else if (Input.GetKey(_isControlByArrow ? KeyCode.UpArrow : KeyCode.W)) {
-            moveVelocity.y += _speedByInput;
+            _moveVelocity.y += _speedByInput;
         }
         if (Input.GetKey(_isControlByArrow ? KeyCode.LeftArrow : KeyCode.A)) {
-            moveVelocity.x -= _speedByInput;
+            _moveVelocity.x -= _speedByInput;
         }
         else if (Input.GetKey(_isControlByArrow ? KeyCode.RightArrow : KeyCode.D)) {
-            moveVelocity.x += _speedByInput;
+            _moveVelocity.x += _speedByInput;
         }
+
     }
 
     private void FixedUpdate()
     {
-        this.transform.position += moveVelocity * Time.deltaTime;
+        _position += _moveVelocity * Time.deltaTime;
+        float angle = _rotation.eulerAngles.z;
+        if (180f < angle) {
+            angle -= 360f;
+        }
+        goalAngle = Mathf.Atan2(_moveVelocity.y, Mathf.Abs(_moveVelocity.x)) * Mathf.Rad2Deg;
+        float rotateY = _moveVelocity.x < 0f ? 180f : 0f;
+        // if (90f < Mathf.Abs(goalAngle)) {
+        //     goalAngle = (180f - Mathf.Abs(goalAngle)) * Mathf.Sign(goalAngle);
+        //     rotateY = 180f;
+        // }
+        goalAngle = Mathf.Clamp(goalAngle, -45f, 45f);
+
+        float diffToGoal = goalAngle - angle;
+        Debug.Log($"{diffToGoal}, {goalAngle}, {angle}");
+        if (Mathf.Abs(diffToGoal) < _rotationSpeedDeg) {
+            angle = goalAngle;
+        } else {
+            angle += Mathf.Sign(diffToGoal) * _rotationSpeedDeg;
+        }
+        _rotation = Quaternion.Euler(0f, rotateY, angle);
+        this.transform.SetPositionAndRotation(_position, _rotation);
     }
 
-    private Vector3 moveVelocity;
+    private Vector3 _moveVelocity;
+    private Vector3 _position;
+    private Quaternion _rotation;
+    [SerializeField]
+    private float goalAngle;
 }
