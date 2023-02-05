3f9bae2e4ab78f756c3c5d316a5bdfef2be3190d
gomaoil <foluken@gmail.com>
2023-02-05 15:39:48 +0900

    Add scene change triggered by game pad;

--- GGJ-2023/Assets/_Project/Scripts/RestartScene.cs
+++ GGJ-2023/Assets/_Project/Scripts/RestartScene.cs
@@ -1,11 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 using UnityEngine.SceneManagement;
 
 public class RestartScene : MonoBehaviour
 {
-        public string sceneName;
+    public string sceneName;
+
+    private InputAction _pressAnyKeyAction =
+                    new InputAction(type: InputActionType.PassThrough, binding: "*/<Button>", interactions: "Press");
+
+    private void OnEnable() => _pressAnyKeyAction.Enable();
+    private void OnDisable() => _pressAnyKeyAction.Disable();
 
     // Start is called before the first frame update
     void Start()
@@ -17,10 +24,10 @@ public class RestartScene : MonoBehaviour
     void Update()
     {
 
-        if (Input.anyKey)
-                {
-                SceneManager.LoadScene(sceneName);
-                }
-        
+        if (Input.anyKey || _pressAnyKeyAction.triggered)
+        {
+            SceneManager.LoadScene(sceneName);
+        }
+
     }
 }
--- GGJ-2023/Assets/_Project/Scripts/SceneChange.cs
+++ GGJ-2023/Assets/_Project/Scripts/SceneChange.cs
@@ -1,13 +1,20 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 using UnityEngine.SceneManagement;
 
 public class SceneChange : MonoBehaviour
 {
     public string sceneName;
 
+    private InputAction _pressAnyKeyAction =
+                    new InputAction(type: InputActionType.PassThrough, binding: "*/<Button>", interactions: "Press");
+
+    private void OnEnable() => _pressAnyKeyAction.Enable();
+    private void OnDisable() => _pressAnyKeyAction.Disable();
+
     private void Update()
     {
-        if (Input.anyKey)
+        if (Input.anyKey || _pressAnyKeyAction.triggered)
         {
             SceneManager.LoadScene(sceneName);
         }
